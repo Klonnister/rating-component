7a24167895b28d248a8567b29ae2f778ada2d435
Dennis Herrera <dennishumher25@hotmail.com>
2023-06-24 22:43:13 -0600

    fixed number buttons events and submit button event

--- src/script.js
+++ src/script.js
@@ -3,13 +3,11 @@ const ratingNumber = document.getElementById("ratingNu");
 
 buttons.forEach((button) => {
     button.addEventListener("mousedown", function() {
-        button.classList.remove("hover");
-        button.classList.remove("clicked");
-        button.classList.add("down");
+      button.classList.add("down");
+      button.classList.remove("hover");
     })
     button.addEventListener("mouseup", function() {
-        button.classList.remove("down");
-        button.classList.add("hover");
+      button.classList.remove("down");
     })
 });
 
@@ -21,12 +19,16 @@ function afterClick(buttonId) {
     if(button.getAttribute("id") != buttonId){
         button.classList.remove("clicked");
     } 
-
   });
-  clickedButton.classList.add("clicked");
-    
 
-  ratingNumber.innerHTML = buttonValue;
+  if(!clickedButton.classList.contains("clicked")){
+    clickedButton.classList.add("clicked");
+    ratingNumber.innerHTML = buttonValue;
+  } else {
+    clickedButton.classList.remove("clicked");
+    clickedButton.classList.add("hover");
+    ratingNumber.innerHTML = "";
+  }
 }
 
 
@@ -35,17 +37,12 @@ function submitAnswer() {
   const thanksS = document.getElementById("thanksS");
   const ratingS = document.getElementById("ratingS");
 
-  buttons.forEach((button) => {
-    if(button.classList.contains("clicked")) {
-      thanksS.style.display = "flex";
-      ratingS.style.display = "none";    
-    } 
-  })
-
-  /*if (ratingNumber.innerText != "" && ratingNumber.innerText != null) {
+  if (ratingNumber.innerText != "" && ratingNumber.innerText != null) {
     thanksS.style.display = "flex";
     ratingS.style.display = "none";
-  }*/
+  } else {
+    alert("Please select a number.")
+  }
 }
 
 document.getElementById("submitStars").addEventListener("click", submitAnswer)
